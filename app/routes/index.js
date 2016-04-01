34b6ac07688c5f1ef2cf56240dbfcb4461739d47
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-01 10:56:08 -0700

    Delete the answer route and combine in the question

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
-    
+
   },
   actions: {
     saveQuestion(params) {
@@ -11,5 +11,19 @@ export default Ember.Route.extend({
       newQuestion.save();
       this.transitionTo('index');
     }
-  }
+  },
 });
+
+// updatePost(entry, params) {
+//       Object.keys(params).forEach(function(key) {
+//         if(params[key] !== undefined) {
+//           entry.set(key, params[key]);
+//         }
+//       });
+//       entry.save();
+//       this.transitionTo('index');
+//     },
+//     deletePost(entry) {
+//       entry.destroyRecord();
+//       this.transitionTo('index');
+//     }    
