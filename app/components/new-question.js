120ce8ba04d23205690e30a74d4fd78a8ff9ff44
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-01 11:02:49 -0700

    Add component new-question with option for show/hide form

---
+++ app/components/new-question.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    questionFormHide() {
+      this.set('addNewQuestion', false);
+    },
+    saveQuestion() {
+      var params = {
+        title: this.get('title')
+        author: this.get('author'),
+        // date: new Date(),
+        information: this.get('information'),
+      };
+      this.set('addNewQuestion', false);
+
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
