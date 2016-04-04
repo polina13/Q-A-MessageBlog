7dbbf489a56a18e431b14c7514d7405666879543
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-04 06:17:43 -0700

    Update hbs pages with more information and details

--- app/routes/answer.js
+++ app/routes/answer.js
@@ -1,8 +1,7 @@
-// import Ember from 'ember';
-//
-// export default Ember.Route.extend({
-//   model(params) {
-//     return this.store.findRecord('answer', params.answer_id);
-//   }
-//
-// });
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('answer', params.answer_id);
+  }
+});
