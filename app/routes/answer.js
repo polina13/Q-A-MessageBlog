c9f77ce5eb96ac85bd88357c08099eb7c425c26f
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-11 07:51:11 -0700

    Update and fix spacing and indentation

--- app/routes/answer.js
+++ app/routes/answer.js
@@ -5,18 +5,3 @@ export default Ember.Route.extend({
     return this.store.findRecord('answer', params.answer_id);
   }
 });
-
-
-// import Ember from 'ember';
-//
-// export default Ember.Route.extend({
-//   model: function() {
-//     var question = this.modelFor('question');
-//     var answer = this.store.createRecord('answer');
-//     question.get('answers').then(function(answers) {
-//       answers.pushObject(answer);
-//       question.save();
-//     });
-//     return answer;
-//     }
-// });
